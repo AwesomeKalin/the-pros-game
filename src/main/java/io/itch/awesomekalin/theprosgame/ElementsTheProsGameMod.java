605e11ce9e8310f7b2dce15c19e116af1211a9e8
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2020-12-24 17:20:35 +0000

    Added recipes for tools and made Pro Chest GUI

--- src/main/java/io/itch/awesomekalin/theprosgame/ElementsTheProsGameMod.java
+++ src/main/java/io/itch/awesomekalin/theprosgame/ElementsTheProsGameMod.java
@@ -45,6 +45,8 @@ import java.util.ArrayList;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Retention;
 
+import io.itch.awesomekalin.theprosgame.gui.GuiProChestGUI;
+
 public class ElementsTheProsGameMod implements IFuelHandler, IWorldGenerator {
 	public final List<ModElement> elements = new ArrayList<>();
 	public final List<Supplier<Block>> blocks = new ArrayList<>();
@@ -125,11 +127,15 @@ public class ElementsTheProsGameMod implements IFuelHandler, IWorldGenerator {
 	public static class GuiHandler implements IGuiHandler {
 		@Override
 		public Object getServerGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
+			if (id == GuiProChestGUI.GUIID)
+				return new GuiProChestGUI.GuiContainerMod(world, x, y, z, player);
 			return null;
 		}
 
 		@Override
 		public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
+			if (id == GuiProChestGUI.GUIID)
+				return new GuiProChestGUI.GuiWindow(world, x, y, z, player);
 			return null;
 		}
 	}
